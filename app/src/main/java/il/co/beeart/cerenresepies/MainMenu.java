8bbeecacf2a24be9f364ce593ab7a155637ed947
danielmedalsi1234 <danielmedalsi1234@gmail.com>
2018-05-31 20:52:02 +0300

    Rec view and add topics

--- app/src/main/java/il/co/beeart/cerenresepies/MainMenu.java
+++ app/src/main/java/il/co/beeart/cerenresepies/MainMenu.java
@@ -1,16 +1,16 @@
 package il.co.beeart.cerenresepies;
 
 import android.app.Activity;
-import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.ListView;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,19 +19,22 @@ public class MainMenu extends Activity {
 
 
     RecyclerView recyclerView;
+    Button BtnAddTopic, BtnOk;
+    EditText txtAddNewTopic;
     private RecyclerView.LayoutManager layoutManager;
     //String[] topicsNames = {"Deserts", "Bakery", "Main Courses", "Breakfasts", "Salads", "soups"};
     List<RecipesTopics> listData = new ArrayList();
 
-    private Button btnAddTopic;
 
-    public MainMenu() {
-    }
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu_layout);
+        BtnAddTopic = findViewById(R.id.BtnAddTopic);
+        BtnOk = findViewById(R.id.BtnOk);
+        txtAddNewTopic = findViewById(R.id.txtAddTopic);
 
 
 
@@ -42,26 +45,32 @@ public class MainMenu extends Activity {
         final RecyclerView.Adapter adapter = new RecyclerAdapter(listData, this);
         recyclerView.setAdapter(adapter);
 
-        listData.add(new RecipesTopics("2"));
 
-            /*btnAddTopic.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    FragmentManager fragmentManager = getFragmentManager();
-                    NewTopicDialog topicDialog =
-                            new NewTopicDialog();
-                    topicDialog.setCancelable(true);
-                    //  topicDialog.setListener(this);
-                    topicDialog.show(fragmentManager, "1");
-                }
-            });*/
-
-        /*lstTopics.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        BtnAddTopic.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+            public void onClick(View v) {
+                txtAddNewTopic.setVisibility(View.VISIBLE);
+                BtnOk.setVisibility(View.VISIBLE);
             }
+        });
 
-        });*/
+        BtnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!txtAddNewTopic.getText().toString().equals("")) {
+                    String input = txtAddNewTopic.getText().toString();
+                    listData.add(new RecipesTopics(input.toString()));
+                    Toast.makeText(MainMenu.this, "Seccessfully add",
+                            Toast.LENGTH_SHORT).show();
+                    txtAddNewTopic.setVisibility(View.INVISIBLE);
+                    BtnOk.setVisibility(View.INVISIBLE);
+                }
+                else
+                    Toast.makeText(MainMenu.this, "must write something",
+                            Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
+
